1e896d1c029bf3acc46e1ca26c99a9e819e24411
Gaston Almiron <gastonfce@gmail.com>
2023-10-17 23:26:12 -0300

    Aplicacion vue base

--- Django + Vue/Api-Transporte/quotesapp/settings.py
+++ Django + Vue/Api-Transporte/quotesapp/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'quotes',
-    'django_filters'
+    'django_filters',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #corsheaders
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"] #corsheaders
 
 ROOT_URLCONF = 'quotesapp.urls'
 
